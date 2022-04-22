35a05047d71e016470c2a4b4055aac6e58caa23c
gokcaneren <gokcann01@gmail.com>
2022-04-22 22:45:23 +0300

    Product Enlarged!

--- WebApiAndEntity/Program.cs
+++ WebApiAndEntity/Program.cs
@@ -22,7 +22,14 @@ using(var scope = app.Services.CreateScope())
     products.Add(new Products { Id = 2, Name = "Desktop", StockAmount = 10 });
     products.Add(new Products { Id = 3, Name = "Keyboard", StockAmount = 25 });
     products.Add(new Products { Id = 4, Name = "Mouse", StockAmount = 43 });
-    products.Add(new Products { Id = 5, Name = "Printer", StockAmount = 3 });
+    products.Add(new Products { Id = 6, Name = "Printer", StockAmount = 3 });
+    products.Add(new Products { Id = 7, Name = "HeadSet", StockAmount = 35 });
+    products.Add(new Products { Id = 8, Name = "Scanner", StockAmount = 2 });
+    products.Add(new Products { Id = 9, Name = "Controller", StockAmount = 15 });
+    products.Add(new Products { Id = 10, Name = "MousePad", StockAmount = 75 });
+    products.Add(new Products { Id = 11, Name = "Usb", StockAmount = 53 });
+    products.Add(new Products { Id = 12, Name = "SSD", StockAmount = 5 });
+    products.Add(new Products { Id = 13, Name = "HDD", StockAmount = 1 });
 
     db.Products.AddRange(products);
     db.SaveChanges();
